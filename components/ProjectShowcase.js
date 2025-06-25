58dce557d5055306c0e6b7c3e70cd923805cb155
dhruvshibhare <shivharedhruv70@gmail.com>
2025-06-25 19:32:30 +0530

    Update ProjectShowcase.js

--- components/ProjectShowcase.js
+++ components/ProjectShowcase.js
@@ -19,7 +19,7 @@ export default function ProjectShowcase() {
       title: 'Lumina Finance',
       category: 'Corporate Website',
       description: 'Modern fintech platform with real-time data visualization and interactive dashboards.',
-      image: 'shop/images/c1.png',
+      image: 'shop/c1.png',
       link: '/projects'
     },
     {
